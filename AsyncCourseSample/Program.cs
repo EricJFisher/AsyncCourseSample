8eb97c8344a342c5d5d2b75fef4c4f774f2c0c9e
Eric J Fisher <eric-fisher@pluralsight.com>
2018-10-18 16:44:45 -0400

    Base Application Initial Checkin

--- AsyncCourseSample/Program.cs
+++ AsyncCourseSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncCourseSample
 {
@@ -6,7 +7,38 @@ namespace AsyncCourseSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task task = Method1Async();
+            task.Wait();
+            Console.ReadLine();
+        }
+
+        static async Task Method1Async()
+        {
+            Task<int> taskOne = SlowMethodOneAsync();
+            Task<int> taskTwo = SlowMethodTwoAsync();
+            int total = await taskOne + await taskTwo;
+            
+            Console.WriteLine("Total: " + total);
+        }
+
+        static async Task<int> SlowMethodOneAsync()
+        {
+            int output = 5;
+
+            await Task.Delay(150);
+            Console.WriteLine("Method One: " + output);
+
+            return output;
+        }
+
+        static async Task<int> SlowMethodTwoAsync()
+        {
+            int output = 2;
+
+            await Task.Delay(50);
+            Console.WriteLine("Method Two: " + output);
+
+            return output;
         }
     }
 }
